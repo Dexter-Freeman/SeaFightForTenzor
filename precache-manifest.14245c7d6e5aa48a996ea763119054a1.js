5f45f8b5846d443c7638e533cda9eb9f82adcee8
Dexter Freeman <vse.pisma.tut@gmail.com>
2020-02-16 21:06:35 +0300

    Updates

---
+++ precache-manifest.14245c7d6e5aa48a996ea763119054a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406dd76fd8a0edf00d7a399bc2932173",
+    "url": "/sea-fight-for-tenzor/index.html"
+  },
+  {
+    "revision": "1f5a7c0d82d034564cf9",
+    "url": "/sea-fight-for-tenzor/static/css/main.6090f0ec.chunk.css"
+  },
+  {
+    "revision": "9b631d639d44daccb457",
+    "url": "/sea-fight-for-tenzor/static/js/2.1cd90ea7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sea-fight-for-tenzor/static/js/2.1cd90ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5a7c0d82d034564cf9",
+    "url": "/sea-fight-for-tenzor/static/js/main.cd9dd0ff.chunk.js"
+  },
+  {
+    "revision": "e5dc27a0a9909da56414",
+    "url": "/sea-fight-for-tenzor/static/js/runtime-main.ad21c08b.js"
+  },
+  {
+    "revision": "f6c7499fc8622c2e9519e41ebdb5076d",
+    "url": "/sea-fight-for-tenzor/static/media/background.f6c7499f.jpg"
+  }
+]);
